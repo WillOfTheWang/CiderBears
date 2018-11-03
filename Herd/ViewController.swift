fb845fbfc457e20324faba87cde305b73867aeb8
xyf199919 <40393702+xyf199919@users.noreply.github.com>
2018-11-03 11:29:38 -0700

    Add files via upload

---
+++ Herd/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Herd
+//
+//  Created by William W. on 11/3/18.
+//  Copyright © 2018 Draphix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func UserName(_ sender: UITextField) {
+    }
+    
+    
+    
+    
+    @IBAction func Code(_ sender: UITextField) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
